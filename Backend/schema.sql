e66d2e36396fa6348cfadf511557e9f15431c47d
Aspectretro <max.zhengsc10@gmail.com>
2026-04-30 20:38:52 +1000

    fix: code format

--- Backend/schema.sql
+++ Backend/schema.sql
@@ -1,26 +1,40 @@
-/* 
-Age group would be defined by intervals, and each would be labeled by alphabetical order
+-- sql queries --
+
+/*
+  Begin Tables
+
+  Age group would be defined by intervals, 
+  and each would be labeled by alphabetical order
 */
+
 CREATE TABLE IF NOT EXISTS user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
-    email TEXT NOT NULL
-    username TEXT NOT NULL
-    password CHAR(128) NOT NULL
-    profession TEXT NOT NULL
-    age_group TEXT NOT NULL
-)
+    user_id       INTEGER  PRIMARY KEY AUTOINCREMENT,
+    email         TEXT     NOT NULL,
+    username      TEXT     NOT NULL,
+    password      CHAR(128) NOT NULL,
+    profession    TEXT     NOT NULL,
+    age_group     TEXT     NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS project (
-    proj_id INTEGER FOREIGN KEY AUTOINCREMENT
-    task_id INTEGER FOREIGN KEY AUTOINCREMENT
-    nember_id INTEGER FOREIGN KEY AUTOINCREMENT
-    title TEXT NOT NULL
-    detail TEXT NOT NULL
-)
+    member_id     INTEGER  PRIMARY KEY AUTOINCREMENT,
+    user_id       INTEGER  NOT NULL,
+    task_id       INTEGER  NOT NULL,
+    title         TEXT     NOT NULL,
+    detail        TEXT     NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (task_id) REFERENCES task(task_id)
+);
 
 CREATE TABLE IF NOT EXISTS task (
-    task_id INTEGER PRIMARY KEY AUTOINCREMENT
-    title TEXT NOT NULL
-    content TEXT NOT NULL
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+    task_id       INTEGER   PRIMARY KEY AUTOINCREMENT,
+    title         TEXT      NOT NULL,
+    content       TEXT      NOT NULL,
+    created_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS member (
+    member_id     INTEGER  PRIMARY KEY AUTOINCREMENT,
+    name          TEXT     NOT NULL,
+    email         TEXT     NOT NULL
+);
