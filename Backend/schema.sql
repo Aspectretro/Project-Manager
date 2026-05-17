e1447f3f89c07408c6cb92ad27ff7b92f0662a19
Aspectretro <max.zhengsc10@gmail.com>
2026-05-17 18:54:08 +1000

    formating

--- Backend/schema.sql
+++ Backend/schema.sql
@@ -1,25 +1,23 @@
 /*
-Begin Tables
-
-Age group would be defined by intervals, 
-and each would be labeled by alphabetical order
-
-Insert new columns in the user table after the profile creation/set-up page is done
+ Begin Tables
+ 
+ Age group would be defined by intervals, 
+ and each would be labeled by alphabetical order
+ 
+ Insert new columns in the user table after the profile creation/set-up page is done
  */
-CREATE TABLE
-    IF NOT EXISTS user (
-        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT NOT NULL,
-        password CHAR(128) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+CREATE TABLE IF NOT EXISTS user (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    password CHAR(128) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE
-    IF NOT EXISTS task (
-        task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        content TEXT NOT NULL,
-        tag TEXT NOT NULL,
-        due_date DATE,
-        created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+CREATE TABLE IF NOT EXISTS task (
+    task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    tag TEXT NOT NULL,
+    due_date DATE,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+); 
