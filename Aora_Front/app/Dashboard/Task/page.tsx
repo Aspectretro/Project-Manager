000f663afcb45a1376b9ca6510cd950b7af32272
Aspectretro <max.zhengsc10@gmail.com>
2026-05-18 14:50:59 +1000

    feat: specific task page

---
+++ Aora_Front/app/Dashboard/Task/page.tsx
@@ -0,0 +1,24 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+export default function TaskPage() {
+
+}
